d981e0c0c6d3074d0b4a795eb6a3c4372bc2c6e9
Arunkumar Sivaramasethu <aruncdm91@gmail.com>
2020-02-17 13:27:19 +0530

    adding css and js files reference changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
-$.getJSON( "https://anapioficeandfire.com/api/books", function( data ) {
+$.getJSON( "https://anapioficeandfire.com/api/books", function( arun ) {
   var items = [];
-  $.each( data, function( key, val ) {
+  $.each( arun, function( key, val ) {
             tr = $('<tr/>');
     tr.append("<td id='" + key + "'>" + val.url.substr(-1) + "</td>");
-            tr.append("<td id='" + key + "'><a href='/got-book-details.html?url="+val.url+"'>" + val.name + "</a></td>");
+            tr.append("<td id='" + key + "'><a href='./got-book-details.html?url="+val.url+"'>" + val.name + "</a></td>");
             tr.append("<td id='" + key + "'>" + val.characters.length + "</td>");
             tr.append("<td id='" + key + "'>" + val.numberOfPages + "</td>");
             $('tbody').append(tr);
